d4cc2057b0c679b7570fced2588273954a002eef
lioravd <113434611+lioravd@users.noreply.github.com>
2023-06-10 18:53:29 +0300

    Add files via upload

--- Salt-Shaker.c
+++ Salt-Shaker.c
@@ -52,9 +52,9 @@ int main(){
                         if ((double) ((rand_r(&seed)) % RAND_MAX) / RAND_MAX < p)
                             passed++;
                     }
-                salt = SALT_INIT - passed;                                                  // calculating the number of salt grains that did not make it through to expiriment again
 
             }
+            salt = SALT_INIT - passed;                                                  // calculating the number of salt grains that did not make it through to expiriment again
 
         }
 
